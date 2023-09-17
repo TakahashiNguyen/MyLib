5f43905eb39b05579bde827bf97ad1c7bcbf5b22
TakahashiNguyen <vietuk.nguyen.git@icloud.com>
2023-09-17 22:27:51 +0700

    update src/v2enlib/__init__.py

--- src/v2enlib/__init__.py
+++ src/v2enlib/__init__.py
@@ -1,4 +1,4 @@
-from v2enlib.language import *
-from v2enlib.gSQL import *
+from v2enlib.language import InputSent, Executor, Translator, Language
+from v2enlib.gSQL import GSQLClass
 from v2enlib.config import config
-from v2enlib.utils import *
+from v2enlib.utils import Debugging, Sound
